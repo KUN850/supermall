943197aa4038fd65b7a60d772d1bffbe3c2b692f
KUN850 <2226800930@qq.com>
2020-12-11 15:59:41 +0800

    '初始化项目'

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+ <Category></Category>
+</template>
+
+<script>
+import Category from "./views/category/Category";
+
+export default {
+  name: 'App',
+  components: {
+   Category
+  }
+}
+</script>
+
+<style>
+
+</style>
